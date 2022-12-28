6f5ce3731e1cc4664360e5edd9fa261c5045bb07
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-28 00:38:43 -0500

    index.html

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
+const express = require('express');
+const app = express();
+app.set("port", process.env.PORT || 4000);
+const cors = require("cors");
+const fetch = (...args) => {
+    import("node-fetch").then(({default: fetch}) => fetch(...args));
+}
+const bodyParser = require('body-parser');
 
+app.use(cors({
+    origin: "*"
+}))
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
 
+app.get("/", (req, res) => {
+    res.redirect("home")
+});
+
+const userController = require('./controllers/userController')
+app.use("/home", userController)
+
+if(!module.parent){
+	app.listen(app.get('port'), () => {
+		console.log(`✅ PORT: ${app.get('port')} 🌟`);
+	})
+};
+
+const CLIENT_ID = "6539597b82efe5bb73ba";
+const CLIENT_SECRET = "5b7167df2910d45ae57c245a7deb5c63a9a1ffbc"
+
+module.exports = app
