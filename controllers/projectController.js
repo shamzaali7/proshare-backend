a4aa27a3e82abe1404867ca5d928e7c23b6ad94a
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-29 16:39:58 -0500

    Project controller get route

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -2,6 +2,15 @@ const express = require("express")
 const router = express.Router()
 const projectModel = require('../models/projectModel')
   
+router.get("/", async (req, res, next) => {
+    try{
+        const getProjects = await projectModel.find({})
+        res.json(getProjects)
+    }catch(err){
+        next(err)
+    }
+})
+
 
 
 module.exports = router
