07f577eb5476fc55d593df2d2ac4572596548c88
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-29 16:58:54 -0500

    Project controller post route

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const router = express.Router()
 const projectModel = require('../models/projectModel')
+const userModel = require("../models/userModel")
   
 router.get("/", async (req, res, next) => {
     try{
@@ -11,6 +12,22 @@ router.get("/", async (req, res, next) => {
     }
 })
 
-
+router.post("/", async (req, res, next) => {
+    try{
+        const user = await userModel.findOne({googleid: req.body.gid})
+        const createProject = await projectModel.create({
+            title: req.body.title,
+            github: req.body.github,
+            deployedLink: req.body.deployedLink,
+            picture: req.body.picture,
+            code: req.body.code,
+            gid: req.body.gid,
+            user: user._id
+        })
+        res.status(200).json(createProject)
+    }catch(err){
+        next(err)
+    }
+})
 
 module.exports = router
