57728c670589393f95d3ef3d6312f1014b3cc490
Hamzaali7 <Hamzaali7@yahoo.com>
2022-12-29 14:00:40 -0500

    Seed Project

--- db/seed.js
+++ db/seed.js
@@ -15,4 +15,20 @@ User.deleteMany({}).then(() => {
     })
 })
 
-
+Project.deleteMany({}).then(async () => {
+    for(i=0; i<projectSeed.length; i++){
+        Project.create({
+            title: projectSeed[i].title,
+            github: projectSeed[i].github,
+            deployedLink: projectSeed[i].deployedLink,
+            picture: projectSeed[i].picture,
+            code: projectSeed[i].code,
+            comments: projectSeed[i].comments,
+            gid: projectSeed[i].gid,
+        }, 
+        async function (err, res){
+            const newProjectUser = await User.findOne({googleid : res.gid});
+            res.user = newProjectUser._id;
+            await res.save();
+        })
+}})
