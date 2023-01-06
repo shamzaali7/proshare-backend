42f2505ad20287f1090051f7de8ec1bd542c49b2
Hamzaali7 <Hamzaali7@yahoo.com>
2023-01-06 02:49:40 -0500

    Code cleanup

--- index.js
+++ index.js
@@ -25,37 +25,3 @@ if(!module.parent){
 };
 
 module.exports = app
-
-// const fetch = (...args) => {
-//     import("node-fetch").then(({default: fetch}) => fetch(...args));
-// }
-// const bodyParser = require("body-parser");
-// app.get("/getAccessToken", async function(req, res){
-//     console.log(req.query.code, CLIENT_ID, CLIENT_SECRET);
-//     // const params = "?client_id=" + CLIENT_ID + "&client_secret=" + CLIENT_SECRET + "&code=" + req.query.code;
-//     await fetch(`https://github.com/login/oauth/access_token?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&code=${req.query.code}`, {
-//         method: "POST",
-//         headers: {
-//             "Accept":  "application/json"
-//         }
-//     })
-//     .then((response) => {response.json()})
-//     .then((data) => {
-//         console.log(data)
-//         res.json(data)
-//     });
-// });
-// app.get("/getUserData", async function(req, res){
-//     req.get("Authorization");
-//     await fetch("https://api.github.com/user", {
-//         method: "GET",
-//         headers: {
-//             "Authorization" : req.get("Authorization")
-//         }
-//     }).then((response) => {
-//         return response.json()
-//     }).then((data) => {
-//         console.log(data)
-//         res.json(data)
-//     })
-// })
